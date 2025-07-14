628b6eee7f79bd85fc99e3b908c0c77d4077c40e
Hanishka2006 <147728059+Hanishka2006@users.noreply.github.com>
2025-07-14 19:57:59 +0530

    basic portfolio with HTML, CSS & JS

---
+++ script.js
@@ -0,0 +1,5 @@
+function showMessage() {
+  const messageBox = document.getElementById("cute-message");
+  messageBox.textContent = "Hi there! Thanks for visiting my first portfolio 💖";
+  messageBox.style.display = "block";
+}
